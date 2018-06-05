fdbfbc77c90bdbf10c7103e7a4cd35a433606d58
David Markel <davidgmarkel@gmail.com>
2018-06-05 02:19:19 +0000

    automatically backed up by learn

--- app/models/superhero.rb
+++ app/models/superhero.rb
@@ -1,14 +1,8 @@
-require 'pry'
-
 class Superhero
   attr_accessor :name, :power, :bio
   @@all = []
 
-  def initialize(name, power, bio)
-    @name = member.super_hero_name
-    @power = member.super_hero_power
-    @bio = member.super_hero_bio
-    @@all << self
+  def initialize
   end
 
   def self.all
